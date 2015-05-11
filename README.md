0567b90ea3eb833384d069704fdf258a6c486d54
lyutian <lyutian623@163.com>
2015-05-11 15:51:06 +0800

    manipulating pixel

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-mango-tree
-==========
-A practice of basic openCV knowledge.
+#OpenCV Library in Action
+Refering book: OpenCV 2 Computer Vision Application Programming Cookbook
 
-Refering to book: OpenCV 2 Computer Vision Application Programming Cookbook
+##Manipulating the Pixel Value
+- Using `Mat::at(int i, int j)` method, just like `image.at<uchar>(j,i)`.
+- Scanning with pointers like `uchar* data = image.ptr<uchar>(j)`.
+- Scanning with iterators.
+
+
--- pixel-operation.cpp
+++ pixel-operation.cpp
@@ -3,7 +3,7 @@
 using namespace cv;
 using namespace std;
 
-// adding salt noise
+// adding salt noises
 void salt(Mat &image, int n)
 {
 	for (int k=0; k<n; k++)
@@ -25,7 +25,7 @@ void salt(Mat &image, int n)
 	}
 }
 
-// scaning image by using point
+// scaning image by using pointers
 void colorReduce(Mat &image, int div)
 {
 	int nr = image.rows;
@@ -46,6 +46,27 @@ void colorReduce(Mat &image, int div)
 				pdata[3*i+1] = pdata[3*i+1]/div*div + div/2;
 				pdata[3*i+2] = pdata[3*i+2]/div*div + div/2;
 			}
+		}
+	}
+}
+
+// scaning image by using iterator
+void colorReduceIterator(Mat &image, int div)
+{
+	Mat_<Vec3b>::iterator it = image.begin<Vec3b>();
+	Mat_<Vec3b>::iterator it_end = image.end<Vec3b>();
+
+	for( ; it!=it_end; ++it)
+	{
+		if (image.channels() ==1)
+		{
+			(*it)[0] = (*it)[0]/div*div + div/2;
+		}
+		else if (image.channels() == 3)
+		{
+			(*it)[0] = (*it)[0]/div*div + div/2;
+			(*it)[1] = (*it)[1]/div*div + div/2;
+			(*it)[2] = (*it)[2]/div*div + div/2;
 		}
 	}
 }
@@ -72,5 +93,12 @@ int main(int argc, char *argv[])
 	colorReduce(img, 128);
 	namedWindow("Reduce color image", WINDOW_AUTOSIZE);
 	imshow("Reduce color image", img);
+	
+	img_origin.copyTo(img);
+	colorReduceIterator(img, 64);
+	namedWindow("Reduce color image -Iterator", WINDOW_AUTOSIZE);
+	imshow("Reduce color image -Iterator", img);
+
 	waitKey(0);
+	return 0;
 }
